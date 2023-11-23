4d20184b8408cc72cbf4a86c54c8ac8c07e1dd0f
EdicsonR <142340694+EdicsonR@users.noreply.github.com>
2023-11-23 08:49:04 -0500

    Update Bars.py

--- SegundoCorte/matplotlib/Bars.py
+++ SegundoCorte/matplotlib/Bars.py
@@ -1,1 +1,30 @@
+#Grafica 4 barras
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.bar(x,y)
+plt.show()
+
+
+#grafica 4 barras horizontales
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.barh(x, y)
+plt.show()
+
+
+#grafica 4 barras en color Rojo
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.bar(x, y, color = "red")
+plt.show()
+
+
+#define el ancho de las barras
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.bar(x, y, width = 0.1)
+plt.show()
+
 
